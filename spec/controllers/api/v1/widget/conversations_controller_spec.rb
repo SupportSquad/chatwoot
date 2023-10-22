71a1f22f67193fd3e9bc13fbbac6cce4f3be0bb3
Shivam Kumar <csautomaytic@gmail.com>
2023-10-22 23:29:02 +0530

    some changes

--- spec/controllers/api/v1/widget/conversations_controller_spec.rb
+++ spec/controllers/api/v1/widget/conversations_controller_spec.rb
@@ -277,7 +277,7 @@ RSpec.describe '/api/v1/widget/conversations/toggle_typing', type: :request do
         expect(response).to have_http_status(:success)
         conversation.reload
         # conversation custom attributes should have "product_name" key with value "Chatwoot"
-        expect(conversation.custom_attributes).to include('product_name' => 'Chatwoot')
+        expect(conversation.custom_attributes).to include('product_name' => 'SupportSquad')
       end
     end
   end
@@ -307,7 +307,7 @@ RSpec.describe '/api/v1/widget/conversations/toggle_typing', type: :request do
         expect(response).to have_http_status(:success)
         conversation.reload
         # conversation custom attributes should not have "product_name" key with value "Chatwoot"
-        expect(conversation.custom_attributes).not_to include('product_name' => 'Chatwoot')
+        expect(conversation.custom_attributes).not_to include('product_name' => 'SupportSquad')
       end
     end
   end
